aa469f4b48857d9853d9c99890a2c70f864fe190
Nícolas Iensen <nicolas@iensen.me>
2014-10-15 19:32:29 -0300

    [#79805878] fix Event after_create

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ActiveRecord::Base
   belongs_to :user
   validates :user, :title, presence: true
   after_create { self.user.delay.create_mailchimp_segment }
+  after_create { self.delay.create_mailchimp_segment }
 
   def create_mailchimp_segment
     segment = Gibbon::API.lists.segment_add(
