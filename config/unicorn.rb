61edc0754fe0148b52d5dd6f3ba0bc858fe7ba7b
Nícolas Iensen <nicolas@iensen.me>
2014-10-15 19:17:13 -0300

    [#79805878] config sidekiq

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -20,11 +20,11 @@ after_fork do |server, worker|
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { :size => 1 }
+    config.redis = { size: 1, url: ENV["REDISTOGO_URL"] }
   end
 
   Sidekiq.configure_server do |config|
-    config.redis = { :size => 5 }
+    config.redis = { size: 5, url: ENV["REDISTOGO_URL"] }
   end
 
   defined?(ActiveRecord::Base) and
