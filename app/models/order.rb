7af8112d4260faaf09190d192ad69dfe7999b54e
Nícolas Iensen <nicolas@iensen.me>
2014-10-17 13:18:31 -0300

    [#79742646] add order reloations to model definition

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,13 @@
 class Order < ActiveRecord::Base
   belongs_to :user
+  has_many :order_items
+  has_many :tickets, through: :order_items
+  has_many :events, through: :tickets
   validates :user, presence: true
+
+  after_create do
+    self.events.each do |event|
+      AddToMailchimpSegmentWorker.perform_async(event.mailchimp_segment_uid, self.user.mailchimp_uid)
+    end
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,5 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Order, :type => :model do
   it { should belong_to :user }
+  it { should have_many :order_items }
+  it { should have_many :tickets }
+  it { should have_many :events }
   it { should validate_presence_of :user }
 end
