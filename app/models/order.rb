95de2804e00354dd199bb886801cf625fc10948d
Nícolas Iensen <nicolas@iensen.me>
2014-10-17 14:05:20 -0300

    [#79742646] add callback to the OrderItem to add user to the event's Mailchimp segment

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,12 +4,4 @@ class Order < ActiveRecord::Base
   has_many :tickets, through: :order_items
   has_many :events, through: :tickets
   validates :user, presence: true
-
-  accepts_nested_attributes_for :order_items
-
-  after_create do
-    self.events.each do |event|
-      AddToMailchimpSegmentWorker.perform_async(event.mailchimp_segment_uid, self.user.mailchimp_uid)
-    end
-  end
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,5 +1,11 @@
 class OrderItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :ticket
+  has_one :event, through: :ticket
+  has_one :user, through: :order
   validates :order, :ticket, presence: true
+
+  after_create do
+    AddToMailchimpSegmentWorker.perform_async(self.event.mailchimp_segment_uid, self.user.mailchimp_uid)
+  end
 end
