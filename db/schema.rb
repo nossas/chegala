c24ca782125c4aa95bd41b6393c65f15ba78dc67
Nícolas Iensen <nicolas@iensen.me>
2014-10-17 11:29:23 -0300

    [#79742646] add orders table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141016160636) do
+ActiveRecord::Schema.define(version: 20141017140403) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +23,12 @@ ActiveRecord::Schema.define(version: 20141016160636) do
     t.string   "title"
     t.string   "mailchimp_segment_uid"
     t.string   "mailchimp_vip_segment_uid"
+  end
+
+  create_table "orders", force: true do |t|
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
