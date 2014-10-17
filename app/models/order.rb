7aeb336345fcf6419692cffdd1d2eafce9cac42d
Josias Schneider <josias.ds@gmail.com>
2014-10-17 17:42:13 -0300

    [#79742620] add user to segment of the event's organizator

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,10 @@ class Order < ActiveRecord::Base
   has_many :tickets, through: :order_items
   has_many :events, through: :tickets
   validates :user, presence: true
+
+  after_create do
+    self.events.each do |event|
+      AddToMailchimpSegmentWorker.perform_async(event.user.mailchimp_segment_uid, self.user.mailchimp_uid)
+    end
+  end
 end
