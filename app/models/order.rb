22ed5d1a93ec6d6190027673933d3f4de4985180
Nícolas Iensen <nicolas@iensen.me>
2014-10-17 13:47:28 -0300

    [#79742646] accepts nested attributes for order items in Order class

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,8 @@ class Order < ActiveRecord::Base
   has_many :events, through: :tickets
   validates :user, presence: true
 
+  accepts_nested_attributes_for :order_items
+
   after_create do
     self.events.each do |event|
       AddToMailchimpSegmentWorker.perform_async(event.mailchimp_segment_uid, self.user.mailchimp_uid)
